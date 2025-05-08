2600f7052506c82b17af2377703dbf4ab0a8da0c
Doug <dm303102@gmail.com>
2025-05-08 12:43:54 -0400

    Update widget.js

--- widget.js
+++ widget.js
@@ -1,1 +1,23 @@
 //TODO
+function svgTextToPngDataUrl(svgText, w, h, callback) {
+  // create a Blob URL for the SVG
+  const blob = new Blob([svgText], { type: 'image/svg+xml' });
+  const url  = URL.createObjectURL(blob);
+
+  const img = new Image();
+  img.onload = () => {
+    // draw into canvas at the requested size:
+    const canvas = document.createElement('canvas');
+    canvas.width  = w;
+    canvas.height = h;
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0, w, h);
+
+    // cleanup
+    URL.revokeObjectURL(url);
+
+    // give back the PNG data‑URL
+    callback(canvas.toDataURL('image/png'));
+  };
+  img.src = url;
+}
