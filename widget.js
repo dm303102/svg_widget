3765d19f318d0bc695c03eb6b2f237ea681b541d
Doug <dm303102@gmail.com>
2025-05-08 12:58:33 -0400

    Update widget.js

--- widget.js
+++ widget.js
@@ -3,11 +3,6 @@ function svgTextToPngDataUrl(svgText, w, h, callback) {
   // create a Blob URL for the SVG
   const blob = new Blob([svgText], { type: 'image/svg+xml' });
   const url  = URL.createObjectURL(blob);
-
-  // parse ?width=500&height=300 from the URL
-  const params = new URLSearchParams(window.location.search);
-  const forcedW = parseInt(params.get('width'));
-  const forcedH = parseInt(params.get('height'));
   
   const img = new Image();
   img.onload = () => {
@@ -26,6 +21,11 @@ function svgTextToPngDataUrl(svgText, w, h, callback) {
   };
   img.src = url;
 }
+
+  // parse ?width=500&height=300 from the URL
+  const params = new URLSearchParams(window.location.search);
+  const forcedW = parseInt(params.get('width'));
+  const forcedH = parseInt(params.get('height'));
 
 if (forcedW && forcedH && uploadedSvgText) {
   svgTextToPngDataUrl(uploadedSvgText, forcedW, forcedH, (pngDataUrl) => {
